17f4477ad16d901a195747178a3f7a9b8cc8b0e1
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-08-11 21:06:24 +0530

    [mohitkumar9884] creating/adding branch and program CreateStackUC1

---
+++ StacksAndQueues/LinkedListStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class LinkedListStack
+    {
+        private Node top;
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = top;
+            top = newNode;
+        }
+
+        public int Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            int data = top.Data;
+            top = top.Next;
+            return data;
+        }
+
+        public int Peek()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            return top.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+    }
+}
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Stacks And Queues Program.");
+            Console.WriteLine("Welcome To Stacks And Queues Program");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            Console.WriteLine("Top element of the stack: " + stack.Peek());   
+
+            Console.WriteLine("Popped: " + stack.Pop());  
+            Console.WriteLine("Popped: " + stack.Pop());  
+            Console.WriteLine("Popped: " + stack.Pop());  
+            Console.WriteLine("Is the stack empty? " + stack.IsEmpty());  
         }
     }
 }
