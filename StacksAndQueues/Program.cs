e9c90368fcfa101c96672a36b1d5a31967f7fda0
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-08-11 20:39:49 +0530

    [mohitkumar9884] creating/adding creating project StacksAndQueues

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StacksAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stacks And Queues Program.");
+        }
+    }
+}
